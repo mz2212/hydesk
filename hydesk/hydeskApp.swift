4612928d1d4745a25732bfef5854d4f64e8869de
Mike <mike@myon.xyz>
2023-12-16 02:33:51 -0700

    Initial Commit

---
+++ hydesk/hydeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydeskApp.swift
+//  hydesk
+//
+//  Created by Michael Pulliam on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct hydeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
